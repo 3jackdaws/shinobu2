47878f498c43faaec399e9c212a88c49e76bfde4
Ian Murphy <3jackdaws@gmail.com>
2018-03-18 18:58:48 -0700

    Add mtg stuffs

--- modules/mtg/mtg.py
+++ modules/mtg/mtg.py
@@ -19,11 +19,28 @@ async def get_image(multiverse_id):
 
 async def card_img_from_name(message:Message, *args):
     name = " ".join(args)
-    print(name)
-    card = Card.where(name=name).all()[0]
-    print(card.name)
-    img = BytesIO()
-    bytes = await get_image(card.multiverse_id)
-    img.write(bytes)
-    img.seek(0)
-    await shinobu.send_file(message.channel, img, filename=f'{card.name}.jpg')
+    cards = Card.where(name=name).all()
+    await shinobu.send_typing(message.channel)
+
+    card_set = []
+    card_names = []
+    for card in cards:
+        if card.multiverse_id and card.name not in card_names:
+            card_set.append(card)
+            card_names.append(card.name)
+
+    if not card_set:
+        return '**No Results**'
+    elif len(card_set) > 1:
+        return '**Search Returned Multiple Results:**\n' + '\n'.join([card.name for card in card_set])
+    else:
+        for card in card_set:
+            id = getattr(card, "multiverse_id")
+            if id:
+                img = BytesIO()
+                print(card.multiverse_id)
+                bytes = await get_image(card.multiverse_id)
+                img.write(bytes)
+                img.seek(0)
+                await shinobu.send_file(message.channel, img, filename=f'{card.name}.jpg')
+                return
